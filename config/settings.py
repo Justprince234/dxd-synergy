ff69645912a05a14298982b2079b6d1b92602d02
princewilljackson <princewilljackson@ymail.com>
2022-06-27 02:00:50 +0000

    staticfiles commited

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-DISABLE_COLLECTSTATIC=True
+DISABLE_COLLECTSTATIC=1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
